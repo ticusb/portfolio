579ad2f47d401af9746f9c7a3627e4cdb44b9744
Ticus Brandt <LeviticusB02@gmail.com>
2022-10-12 16:59:21 -0600

    Add to GitHub

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Home() {
+
+    return (
+        <section class="home">
+            <div>
+                <h1>I'm Ticus</h1>
+                <h3>Welcome to my page</h3>
+                <button>Lets Explore!</button>
+            </div>
+        </section>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function About() {
+    return (
+        <div class="navbar">
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Explore</li>
+                    <li>About</li>
+                    <li>Contact Me</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
