1669ff1481fab854bb11e0218355aa7e0d43d436
ticusb <leviticusb02@gmail.com>
2022-10-13 01:04:36 -0600

    rename NavBar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Home from "./components/Home.js";
-import Navbar from "./components/NavBar.js";
+import NavBar from "./components/NavBar.js";
 import About from "./components/About.js";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
+      <NavBar/>
       <Home />
       <About />
     </div>
