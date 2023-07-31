443591330e05ff6c4a3637fb58f29c6b5fa35eb7
ticus <90355782+ticusb@users.noreply.github.com>
2023-07-30 19:26:14 -0600

    Added Origin Section

--- src/components/Origin.js
+++ src/components/Origin.js
@@ -4,7 +4,13 @@ import "./styles/Origin.css";
 const Origin = () => {
     return (
         <div className="Origin" id="Origin">
-            <h2>Country of Origin</h2>
+            <p className='facts'>So basically, I'm from Alaska, I play Rocket League, and love the Minions.</p>
+            <div className='img-group'>
+                <img id="RL-img" src='img/RL_Octane.png' alt='Rocket League Car' />
+                <img id="Minion-img" src='img/Minion_Kevin.png' alt='Kevin' />
+            </div>
+            <p className='career'>I also like to consider myself a developer. I'm not a phony I promise.</p>
+
         </div>
     );
 }
