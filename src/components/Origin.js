4e7a422db7f7aafecf0af0138ca6d80411174494
ticus <90355782+ticusb@users.noreply.github.com>
2023-08-01 18:06:01 -0600

    Added project cards

--- src/components/Origin.js
+++ src/components/Origin.js
@@ -4,6 +4,7 @@ import "./styles/Origin.css";
 const Origin = () => {
     return (
         <div className="Origin" id="Origin">
+            <h1 className='Origin-Header'>Country of<br/> Origin</h1>
             <p className='facts'>So basically, I'm from Alaska, I play Rocket League, and love the Minions.</p>
             <div className='img-group'>
                 <img id="RL-img" src='img/RL_Octane.png' alt='Rocket League Car' />
